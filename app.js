d0b203aa71d075aca115ea86feeec960596e53dc
samuelcardoso <samuelparacontato@gmail.com>
2019-10-21 14:38:23 -0300

    ...

--- app.js
+++ app.js
@@ -3,12 +3,18 @@ const app = require('express')(),
   io = require('socket.io')(server),
   rtsp = require('rtsp-ffmpeg');
 server.listen(6147);
-var uri = 'rtsp://wowzaec2demo.streamlock.net/vod/mp4:BigBuckBunny_115k.mov',
-  stream = new rtsp.FFMpeg({input: uri});
+var uri = 'rtsp://admin:Entech2018@aiplatesbetim.ddns.net:554/live2.sdp',
+  stream = new rtsp.FFMpeg({
+    input: uri // stream uri
+    , rate: 30 // output framerate (optional)
+    , resolution: '640x480' // output resolution in WxH format (optional)
+    , quality: 3 // JPEG compression quality level (optional)
+  });
 console.debug('initializing socket...');
 io.on('connection', function(socket) {
   console.debug('socket ok!');
   var pipeStream = function(data) {
+    console.debug(data);
     socket.emit('data', data.toString('base64'));
   };
   stream.on('data', pipeStream);
